218b38293702bf0f9e3f785dd3fbdcad22ac82e4
Kam Banwait <kam@scriptedpixels.co.uk>
2022-08-30 21:53:12 +0100

    Updates netlify function with correct js for API call to endpoint using env var

--- netlify/functions/funfactapi/funfactapi.js
+++ netlify/functions/funfactapi/funfactapi.js
@@ -1,39 +1,26 @@
+const axios = require('axios')
+
 exports.handler = async function() {
   const { VITE_API_KEY } = process.env
   const URL = `https://random-facts4.p.rapidapi.com/get`
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: VITE_API_KEY }),
+  try {
+    const { data } = await axios.get('https://random-facts4.p.rapidapi.com/gets', {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': VITE_API_KEY,
+        'X-RapidAPI-Host': 'random-facts4.p.rapidapi.com'
+      }
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify({ status, statusText, headers, data })
+    }
   }
-
-  // try {
-  //   const data= await fetch('https://random-facts4.p.rapidapi.com/get', {
-  //     method: 'GET',
-  //     headers: {
-  //       'X-RapidAPI-Key': VITE_API_KEY,
-  //       'X-RapidAPI-Host': 'random-facts4.p.rapidapi.com'
-  //     }
-  //     })
-  //     .then(response => response.json())
-  //     .then(response => {
-  //       funFact.value = response[0].description
-  //       loadingNewFunFact.value = false
-  //     })
-  //     .catch(error => {
-  //       funFact.value = error
-  //       loadingNewFunFact.value = false
-  //     })
-
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify(data)
-  //   }
-
-  // } catch (error) {
-  //   return {
-  //     statusCode: error,
-  //     body: JSON.stringify({ error })
-  //   }
-  // }
 }
