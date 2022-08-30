c590f8f54de5c10dbf3e75e8b09587d829ef959d
Kam Banwait <kam@scriptedpixels.co.uk>
2022-08-30 12:20:04 +0100

    imports the new FunFact component. Uses Suspense for initial component load

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
-  <h1 class="text-3xl font-bold underline mb-10">Hello Vue!</h1>
-  <button class="btn btn-primary">Hello Daisy button</button>
+  <main class="container max-w-3xl p-2 flex gap-10 flex-col overflow-auto">
+    <h1 class="text-8xl text-center font-bold pulse">Fun Fact!</h1>
+    <Suspense>
+      <FunFact />
+      <template #fallback>
+        <button class="btn btn-ghost btn-disabled bg-transparent loading btn-xl pulse text-center w-100 text-xl">Loading a Fun Fact...</button>
+      </template>
+    </Suspense>
+  </main>
 </template>
+
+<script setup>
+  import FunFact from '@/components/FunFact.vue'
+</script>
