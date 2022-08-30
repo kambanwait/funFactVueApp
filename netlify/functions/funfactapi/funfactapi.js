aa2eb961a7d767671778698e73dc129814d3e122
Kam Banwait <kam@scriptedpixels.co.uk>
2022-08-30 14:33:37 +0100

    Adds new netlify function

---
+++ netlify/functions/funfactapi/funfactapi.js
@@ -0,0 +1,35 @@
+exports.handler = async function() {
+  const { VITE_API_KEY } = process.env
+  const URL = `https://random-facts4.p.rapidapi.com/get`
+
+  try {
+    const data= await fetch('https://random-facts4.p.rapidapi.com/get', {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': VITE_API_KEY,
+        'X-RapidAPI-Host': 'random-facts4.p.rapidapi.com'
+      }
+      })
+      .then(response => response.json())
+      .then(response => {
+        funFact.value = response[0].description
+        loadingNewFunFact.value = false
+      })
+      .catch(error => {
+        funFact.value = error
+        loadingNewFunFact.value = false
+      })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify({ status, statusText, headers, data })
+    }
+  }
+}
